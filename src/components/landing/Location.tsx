4e83e0057e2a196324aa23153aca32dfca885d15
Tejas Bhati <tejasbhati18@gmail.com>
2025-07-31 09:30:26 +0000

    https://www.google.com/maps/place/Asilo+Girls+PG/@28.4784904,77.5058117,

--- src/components/landing/Location.tsx
+++ src/components/landing/Location.tsx
@@ -42,7 +42,7 @@ export default function Location() {
           <div className="rounded-lg overflow-hidden shadow-lg h-96">
             <a href="https://maps.app.goo.gl/J7brRaJaaQrB2Yrt9" target="_blank" rel="noopener noreferrer" className="block w-full h-full">
                 <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3506.332352812014!2d77.4569650742969!3d28.50058869018414!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ce8677a28849b%3A0x8c79261a8f6e8587!2sAsilo%20PG!5e0!3m2!1sen!2sin!4v1722442456485!5m2!1sen!2sin"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3506.402450332219!2d77.50359197429686!3d28.498114990237464!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ceb5e6bcd9b89%3A0x9caf31c42bc35918!2sAsilo%20Girls%20PG!5e0!3m2!1sen!2sin!4v1722444633215!5m2!1sen!2sin"
                 width="100%"
                 height="100%"
                 style={{ border: 0 }}
